b7d950eb12aa021ecca053d1a408515d3c4ee3a7
AVGuryanov <work.guryanov.andrey@gmail.com>
2025-09-01 10:02:36 +0300

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,20 @@ import cookieParser from 'cookie-parser';
 import router from '@src/router/index.js';
 import cors from 'cors';
 import qs from 'qs';
+import { join, dirname } from 'node:path';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import { fileURLToPath } from 'url';
+
 import { NextFunction, Request, Response } from "express";
 
 const app = express();
 
+const scrDirname = dirname(fileURLToPath(import.meta.url));
+
+const swaggerDoc = YAML.load(join(scrDirname, 'swagger', 'songs.yaml'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.enable('trust proxy');
 app.use(
   cors({
@@ -27,10 +37,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api', router);
 
-app.use(function(error: { code: number; error: unknown }, _req: Request, res: Response, _next: NextFunction) {
+app.use(function(error: { code: number; error: unknown; message?: string }, _req: Request, res: Response, _next: NextFunction) {
   const statusCode = error?.code || 500;
+  const responseError = error?.error || { msg: error?.message || 'internal server error' };
 
-  res.status(statusCode).send(error);
+  res.status(statusCode).send({ error: responseError });
 });
 
 const port = process.env.PORT;
